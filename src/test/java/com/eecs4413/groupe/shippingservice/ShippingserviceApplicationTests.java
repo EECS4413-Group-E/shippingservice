0e68fdd71e4b9c5171e669c6d12508a1f613b4b2
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2026-06-20 21:22:15 -0400

    Generated project with spring initializr

---
+++ src/test/java/com/eecs4413/groupe/shippingservice/ShippingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eecs4413.groupe.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
